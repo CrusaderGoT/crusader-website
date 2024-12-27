adbfb91414d3f5e90b19bcb185e7fc9db33dc522
Emeka <133652764+CrusaderGoT@users.noreply.github.com>
2024-12-28 00:02:57 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,3 @@ export default function Home() {
     <Shell></Shell>
   )
 }
-
-function Main() {
-  return (
-    <div className={`
-    bg-white h-full w-full flex-grow relative
-    `}>
-      <Image
-      src={"/images/nav_pimg.jpg"}
-      height={480}
-      width={636}
-      alt="my navbar profile image"
-      className="h-full w-auto absolute right-0 object-contain"
-      />
-    </div>
-  )
-}
