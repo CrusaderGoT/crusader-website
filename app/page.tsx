eac82d957614c3b9ee05f11ff67eb8c2d77ff7cc
crusader <enememeka44@gmail.com>
2024-12-28 22:21:25 +0100

    added carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,40 @@
-import { Shell } from "./ui/shell";
+"use client"
 
+import { useRef } from 'react';
+import Autoplay from 'embla-carousel-autoplay';
+import Image from 'next/image';
+import { Carousel } from '@mantine/carousel';
+
+
+const images = [
+  "/images/nav_pimg.jpg",
+  "/images/nav_pimg.jpg",
+  "/images/nav_pimg.jpg"
+];
 
 export default function Home() {
-  return (
-    <Main />
-  )
+  return <Main />;
 }
 
 function Main() {
+  const autoplay = useRef(Autoplay({ delay: 2000 }));
+
   return (
-    <div className={`
-    bg-white h-full w-full
-    `}>
-      MAin
-    </div>
-  )
+    <Carousel
+      plugins={[autoplay.current]}
+      onMouseEnter={autoplay.current.stop}
+      onMouseLeave={autoplay.current.reset}
+    >
+      {images.map((url, index) => (
+        <Carousel.Slide key={index}>
+          <Image
+            src={url}
+            alt="image"
+            width={480} // Specify appropriate width
+            height={636} // Specify appropriate height
+          />
+        </Carousel.Slide>
+        ))}
+    </Carousel>
+  );
 }
