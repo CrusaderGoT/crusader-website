89324429e716f301c2ca7ec8a4882fc0bbd03e07
crusader <enememeka44@gmail.com>
2025-01-01 03:37:49 +0100

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,33 @@
+import { Text } from "@mantine/core"
+
 export default function About() {
     return (
         <div>
-            <p>About page</p>
+            <Text>
+            👋 Hi, I&n2019;m @CrusaderGoT
+
+👤 Real Name: Emeka ~ a-may-car
+👀 I’m passionate about Python, especially with Django for web development, and I enjoy working with various libraries and technologies including:
+
+🎶 Librosa for audio and music analysis,
+🎥 Moviepy for video editing and manipulation,
+🔢 Numpy and Scipy for scientific computing,
+🖼️ PIL (Python Imaging Library) for image processing,
+📊 Matplotlib for data visualization,
+💻 HTML and CSS for frontend development,
+🅱️ Bootstrap5 for responsive design.
+🌱 Currently, I'm expanding my skills into JavaScript and diving deeper into Django REST framework for building RESTful APIs. I've also been experimenting with Tailwind CSS for modern and utility-first styling, and Geopy for geocoding and location-based services.
+
+💡 In my recent ventures, I've explored database management with PostgreSQL using Psycopg2, deploying applications on platforms like Render, and API Integration.
+
+🔭 Additionally, some of my personal projects not on GitHub, include an e-commerce website available on sale on Upwork; utilizing APIs from platforms like Reddit, Spotify, Twitter, and Meta for personal use.
+
+🚀 Looking ahead, I aim to delve into cloud technologies like AWS, containerization with Docker, and dive deeper into machine learning (ML) and artificial intelligence (AI).
+
+📫 You can reach me at enememeka44@gmail.com.
+
+Let's connect and collaborate on exciting projects!
+            </Text>
         </div>
     )
 }
