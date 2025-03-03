fec2b170deb99451d255ef9f614edc3db636b867
Emeka <133652764+CrusaderGoT@users.noreply.github.com>
2025-03-03 17:50:34 +0100

    Update Services.tsx
    changed tech slide speed

--- ui/Services.tsx
+++ ui/Services.tsx
@@ -153,7 +153,7 @@ function TechSlides() {
                 const autoscroll = useRef(
                     AutoScroll({
                         startDelay: 1,
-                        speed: 10,
+                        speed: 1,
                         stopOnInteraction: false,
                         stopOnFocusIn: false,
                         direction: index % 2 === 0 ? "forward" : "backward",
