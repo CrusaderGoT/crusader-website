e6b2f5c7374adc6d8ac154275d609d1035a73db7
Emeka <133652764+CrusaderGoT@users.noreply.github.com>
2025-04-04 02:10:08 +0100

    Delete app/(website)/template.tsx

--- app/(website)/template.tsx
+++
@@ -1,9 +0,0 @@
-import { PageTransition } from "@/components/shell/PageTransition";
-
-export default async function Template({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return <PageTransition>{children}</PageTransition>;
-}
