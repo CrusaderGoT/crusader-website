f97595d84c694dd4e9553773d17f82bcd0d3dad2
CrusaderGoT <enememeka44@gmail.com>
2025-05-23 00:42:03 +0100

    minor changes

--- components/data/profileData.ts
+++ components/data/profileData.ts
@@ -9,4 +9,6 @@ export const profileData = {
     email: "enememeka@gmail.com",
     phone: "+234 704 076 4431",
     picture: "/images/nav_pimg.jpg",
+    background:
+        "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop",
 };
