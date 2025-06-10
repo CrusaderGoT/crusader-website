f58980bce24dbd9c76c835e8be963bea959e1967
Emeka <133652764+CrusaderGoT@users.noreply.github.com>
2025-06-10 08:01:52 +0100

    Update profileData.ts

--- components/data/profileData.ts
+++ components/data/profileData.ts
@@ -8,7 +8,7 @@ export const profileData = {
     likes: ["chess", "movies", "writing fiction"],
     email: "enememeka@gmail.com",
     phone: "+234 704 076 4431",
-    picture: "/images/nav_pimg.jpg",
+    picture: "https://res.cloudinary.com/dd9xwf9wk/image/upload/v1731937774/website/profile_picture/website/profile_picture/enemchukwu_chukwuemeka_picture.jpg",
     background:
         "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop",
 };
